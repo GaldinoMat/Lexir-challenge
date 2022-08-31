9f73ecba303837a5cc48b616cdb375de33ccda02
Galdino <mateusgaldino1000@hotmail.com>
2022-08-30 22:07:37 -0300

    feat: :sparkles: add spinner

--- src/components/pages/index/ProductListing/index.tsx
+++ src/components/pages/index/ProductListing/index.tsx
@@ -1,19 +1,37 @@
 import ProductCard from '@components/global/ProductCard'
 import React, { useEffect, useState } from 'react'
 import { IProductObject } from './types/interfaces'
+import ClipLoader from "react-spinners/ClipLoader";
 
 function ProductListing() {
   const [productsData, setProductsData] = useState<IProductObject[]>([])
+  const [loading, setLoading] = useState(true)
 
   const fetchData = async () => {
-    return await fetch('/api/products', {
-      method: 'POST',
-    }).then(resp => resp.json()).then(json => JSON.parse(json))
+    try {
+      return await fetch('/api/products', {
+        method: 'POST',
+      }).then(resp => resp.json()).then(json => {
+        setLoading(false)
+        return JSON.parse(json)
+      })
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   useEffect(() => {
-    fetchData().then((data) => setProductsData(data))
+    fetchData().then(data => setProductsData(data))
   }, [])
+
+  if (loading) return (
+    <div className='w-full h-screen laptop:w-2/3 flex items-center justify-center'>
+      <ClipLoader
+        loading
+        size={75}
+      />
+    </div>
+  )
 
   return (
     <section className='mt-[21px] laptop:mt-0 mb-20 w-full laptop:w-2/3 h-full flex flex-col md:flex-row md:flex-wrap items-center gap-y-10 md:gap-x-14 laptop:gap-5 desktop:gap-9 justify-center desktop:justify-evenly laptop:ml-[21px]'>
